a90915fce7b9301727353c8c49c185866ce60282
Jeremy Schlatter <jeremy.schlatter@gmail.com>
2017-11-13 17:03:01 -0800

    longer demos

--- demo.py
+++ demo.py
@@ -6,7 +6,7 @@ env = GatheringEnv(n_agents=2)
 env.reset()
 env.render()
 
-for _ in range(1000):
+for _ in range(10000):
     observation_n, reward_n, done_n, info_n = env.step(env.action_space.sample())
     if any(done_n):
         break
